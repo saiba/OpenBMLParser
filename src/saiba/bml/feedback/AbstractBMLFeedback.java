bb10020b06f377573dc626f597243afe140bcefe
Herwin van Welbergen <hvanwelbergen@techfak.uni-bielefeld.de>
2014-12-21 16:59:06 +0100

    - Added license to jar distribution - Removed use of deprecated Lombok functionality

--- src/saiba/bml/feedback/AbstractBMLFeedback.java
+++ src/saiba/bml/feedback/AbstractBMLFeedback.java
@@ -1,14 +1,17 @@
 package saiba.bml.feedback;
 
-import java.util.List;
-
-import com.google.common.collect.ImmutableList;
-
-import lombok.Delegate;
-import saiba.bml.core.CustomAttributeHandler;
 import hmi.xml.XMLFormatting;
 import hmi.xml.XMLNameSpace;
 import hmi.xml.XMLStructureAdapter;
+import hmi.xml.XMLTokenizer;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+import saiba.bml.core.CustomAttributeHandler;
+
+import com.google.common.collect.ImmutableList;
 
 /**
  * Skeleton class for BMLFeedback
@@ -17,8 +20,58 @@ import hmi.xml.XMLStructureAdapter;
  */
 public class AbstractBMLFeedback extends XMLStructureAdapter implements BMLFeedback
 {
-    @Delegate
     protected CustomAttributeHandler caHandler = new CustomAttributeHandler();
+
+    public float getCustomFloatParameterValue(String name)
+    {
+        return caHandler.getCustomFloatParameterValue(name);
+    }
+
+    public void addCustomStringParameterValue(String name, String value)
+    {
+        caHandler.addCustomStringParameterValue(name, value);
+    }
+
+    public void addCustomFloatParameterValue(String name, float value)
+    {
+        caHandler.addCustomFloatParameterValue(name, value);
+    }
+
+    public String getCustomStringParameterValue(String name)
+    {
+        return caHandler.getCustomStringParameterValue(name);
+    }
+
+    public boolean specifiesCustomStringParameter(String name)
+    {
+        return caHandler.specifiesCustomStringParameter(name);
+    }
+
+    public boolean specifiesCustomFloatParameter(String name)
+    {
+        return caHandler.specifiesCustomFloatParameter(name);
+    }
+
+    public boolean specifiesCustomParameter(String name)
+    {
+        return caHandler.specifiesCustomParameter(name);
+    }
+
+    public boolean satisfiesCustomConstraint(String name, String value)
+    {
+        return caHandler.satisfiesCustomConstraint(name, value);
+    }
+
+    public void decodeCustomAttributes(HashMap<String, String> attrMap, XMLTokenizer tokenizer, Set<String> floatAttributes,
+            Set<String> stringAttributes, XMLStructureAdapter beh)
+    {
+        caHandler.decodeCustomAttributes(attrMap, tokenizer, floatAttributes, stringAttributes, beh);
+    }
+
+    public StringBuilder appendCustomAttributeString(StringBuilder buf, XMLFormatting fmt)
+    {
+        return caHandler.appendCustomAttributeString(buf, fmt);
+    }
 
     public static final double UNKNOWN_TIME = -Double.MAX_VALUE;
     
